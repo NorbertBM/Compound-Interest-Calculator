ee85b9cbe6336b226157d43297a7f73a74c8b8ef
Norbert B. Menyhart <72194361+NorbertBM@users.noreply.github.com>
2022-10-15 11:18:13 +0200

    update

--- script.js
+++ script.js
@@ -1,52 +1,69 @@
+// Get all elements from the dom using id
+
 const compoundInterestCalculator = document.getElementById(
   "CompoundInterestCalculator"
 );
+
 const initialBalance = document.getElementById("InitialBalance");
 const interestRate = document.getElementById("InterestRate");
-const years = document.getElementById("Years");
+const year = document.getElementById("Years");
 const monthlyContribution = document.getElementById("MonthlyContribution");
-
 const result = document.getElementById("Result");
 
+
+// Form validation and submission
 compoundInterestCalculator.addEventListener("submit", function (e) {
   e.preventDefault();
+  //   console.log(e);
   if (
-    (initialBalance.value === "", interestRate.value === "", years.value === "")
+    initialBalance.value === "" ||
+    interestRate.value === "" ||
+    year.value === ""
   ) {
     alert("Complete all fields");
   } else {
+    // console.log("valid");
     calculateCompoundInterest();
   }
 });
 
+// Calculate compound interest
+
 function calculateCompoundInterest() {
+  // Add fade effect
+
   result.classList.add("fade");
-  setTimeout(() => {
+  setTimeout(function () {
     result.classList.remove("fade");
   }, 1000);
   if (monthlyContribution.value === "") {
     return (result.innerHTML =
-      compoundInterest(initialBalance.value, interestRate.value, years.value) +
+      compoundInterest(initialBalance.value, interestRate.value, year.value) +
       "$");
   } else {
     return (result.innerHTML =
       compoundInterestWithContribution(
         initialBalance.value,
         interestRate.value,
-        years.value,
+        year.value,
         monthlyContribution.value
       ) + "$");
   }
 }
 
 
+
 function compoundInterest(principleAmount, interestRate, years) {
   console.log("simple compounding");
-  console.log(Math.pow(1 + interestRate / 100 / 12, 12 * years).toFixed(2));
+
   return (
     principleAmount * Math.pow(1 + interestRate / 100 / 12, 12 * years)
   ).toFixed(2);
 }
+
+// compound Interest With Contribution
+
+
 
 
 function compoundInterestWithContribution(
@@ -55,14 +72,10 @@ function compoundInterestWithContribution(
   years,
   monthlyContribution
 ) {
-  console.log("compounding with contribution");
-
   return (
     principleAmount * Math.pow(1 + interestRate / 100 / 12, 12 * years) +
-    monthlyContribution *
-      ((Math.pow(1 + interestRate / 100 / 12, 12 * years) - 1) /
-        (interestRate / 100 / 12))
+    (monthlyContribution *
+      (Math.pow(1 + interestRate / 100 / 12, 12 * years) - 1)) /
+      (interestRate / 100 / 12)
   ).toFixed(2);
 }
-
-
